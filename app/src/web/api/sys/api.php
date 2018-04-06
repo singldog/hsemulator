c59a8255441c93db2a2eb5cec8186c5a30bd07e3
Byron Chʌtz <chutz@foxmail.com>
2018-04-06 14:55:08 +0800

    注释

--- app/src/web/api/sys/api.php
+++ app/src/web/api/sys/api.php
@@ -11,15 +11,23 @@ foreach(scandir($apiDir) as $group){
         if($api == "." || $api == "..") continue;
         $url = $apiDir."/".$group."/".$api;
         $file = file_get_contents($url);
-        $attr = @explode("\r\n * @", explode("\r\n */", explode("/**\r\n * @", $file)[1])[0]);
+        $attrs = @explode("\r\n * @", explode("\r\n */", explode("/**\r\n * @", $file)[1])[0]);
         $url = $group."/".str_replace(".php", "", $api);
         $obj = [
             "url" => $url
         ];
-        foreach($attr as $a){
+        foreach($attrs as $attr){
             $aa = explode(" ", $a);
             if(count($aa)==2){
-                $obj[$aa[0]] = $aa[1];
+                if(array_key_exists($aa[0], $obj)){
+                    if(is_array($obj[$aa[0]])){
+                        $obj[$aa[0]] = array_merge([$aa[1]], $obj[$aa[0]]);
+                    }else{
+                        $obj[$aa[0]] = array_merge([$aa[1]], [$obj[$aa[0]]]);
+                    }
+                }else{
+                    $obj[$aa[0]] = $aa[1];
+                }
             }
         }
         $apis[$group][] = $obj;

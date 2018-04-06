d73ce8481866f73f8e98e861078ce7e2597a92eb
Byron Chʌtz <chutz@foxmail.com>
2018-04-06 12:19:53 +0800

    ajax跨域

--- app/index.php
+++ app/index.php
@@ -7,5 +7,10 @@ $loader->setPsr4('app\\', array(__DIR__.'/src'));
 //引入需要加载的文件
 require 'includes.php';
 
+//允许跨域
+header("Access-Control-Allow-Origin:*");
+header("Access-Control-Allow-Methods:GET,POST");
+header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
+
 //运行api
 (new \app\web\Api)->run();
